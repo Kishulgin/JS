6b9de016b0eb06cb16a3cbeb812e623451070b8e
Kishulgin <kishulgin@gmail.com>
2018-04-10 11:39:38 +0300

    11

--- less11/js/script.js
+++ less11/js/script.js
@@ -126,6 +126,8 @@ window.addEventListener ('DOMContentLoaded', () => {
         overlay.style.display = "none";
         document.body.style.overflow ="";
     });
+
+
   let message = new Object();
     message.loading = "Загрузка...";
     message.success = "Спасибо! Скоро мы с вами свяжемся";
@@ -135,7 +137,76 @@ window.addEventListener ('DOMContentLoaded', () => {
         input = document.getElementsByTagName('input'),
         statusMessage = document.createElement('div');
         statusMessage.classList.add('status');
-        console.log(form)
+        contacts = document.getElementsByClassName('contact-form')[0]
+   
+
+
+
+
+        contacts.addEventListener('submit', function(event) {
+            event.preventDefault();
+            contacts.appendChild(statusMessage);
+
+          let request = new XMLHttpRequest();
+            request.open("POST", 'server.php')
+
+            request.setRequestHeader('Content-Type', "application/x-www-form-urlencoded" );
+
+            let formData = new FormData(form);
+            request.send(formData);
+
+
+            request.onreadystatechange = function() {
+                if (request.readyState < 4) {
+                    statusMessage.innerHTML = message.loading;
+                } else if (request.readyState === 4) {
+                    if(request.status == 200 && request.status < 300) {
+                        statusMessage.innerHTML = message.success;
+                    }
+                    else {
+                      statusMessage.innerHTML = message.failure;
+                    }
+                }
+            }
+for (let i = 0; i < input.length; i++) {
+    input[i].value = '';
+}
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         form.addEventListener('submit', function(event) {
             event.preventDefault();
